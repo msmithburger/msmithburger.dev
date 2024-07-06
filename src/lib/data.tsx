61e7156a7d565f49cdf3d15153ad577b71582610
Matt Smithburger <smithburger.matthew@gmail.com>
2024-07-06 13:15:13 -0400

    fix: testimonials build error

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -324,26 +324,4 @@ export const PROJECTS: ProjectDetails[] = [
   },
 ];
 
-// export const TESTIMONIALS: TestimonialDetails[] = [
-//   {
-//     personName: "Krisztian Gyuris",
-//     personAvatar: AvatarKrisztian,
-//     title: "Founder - inboxgenie.io",
-//     testimonial:
-//       "Job well done! I am really impressed. He is very very good at what he does:) I would recommend Matt and will rehire in the future for Frontend development.",
-//   },
-//   {
-//     personName: "Eugen Esanu",
-//     personAvatar: AvatarEugen,
-//     title: "Founder - shosho.design",
-//     testimonial:
-//       "Great guy, highly recommended for any COMPLEX front-end development job! His skills are top-notch and he will be an amazing addition to any team.",
-//   },
-//   {
-//     personName: "Joe Matkin",
-//     personAvatar: AvatarDummy,
-//     title: "Freelancer",
-//     testimonial:
-//       "Matt was extremely easy and pleasant to work with and he truly cares about the project being a success. Matt has a high level of knowledge and was able to work on my MERN stack application without any issues.",
-//   },
-// ];
+export const TESTIMONIALS: TestimonialDetails[] = [];
